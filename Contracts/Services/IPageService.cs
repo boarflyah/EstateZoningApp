8d30a4e5ae9db40f9d32663f05add7d43d0b58c7
Arek Stepnik <arkadiusz.stepnik88@gmail.com>
2023-04-08 17:45:36 +0200

    Repository creation - EstateZoningApp

---
+++ Contracts/Services/IPageService.cs
@@ -0,0 +1,6 @@
+﻿namespace EstateZoningApp.Contracts.Services;
+
+public interface IPageService
+{
+    Type GetPageType(string key);
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EstateZoningApp.ViewModels;
+
+public partial class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    string test;
+}
